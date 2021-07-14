fd35c1e23df1b0677fea4c45bf26943bc6546d66
unknown <theus.7@hotmail.com>
2021-07-14 00:27:27 -0300

    Create menu context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import Routes from './routes'
-import { ThemeProvider, createTheme } from '@material-ui/core/styles';
-import MenuContextProvider from './context/MenuContext';
-function App() {
+import Routes from "./routes";
+import { ThemeProvider, createTheme } from "@material-ui/core/styles";
+import MenuContextProvider from "./context/MenuContext";
 
+function App() {
   const theme = createTheme({
     palette: {
       primary: {
-        main: '#3f51b5',
+        main: "#3f51b5",
       },
       secondary: {
-        main: '#2196f3',
+        main: "#2196f3",
       },
       error: {
-        main: '#f44336',
+        main: "#f44336",
       },
       background: {
-        main: '#f4f8fb',
-        dark: '#202634',
-        lightDark: '#293042'
+        main: "#f4f8fb",
+        dark: "#202634",
+        lightDark: "#293042",
       },
     },
   });
--- src/context/MenuContext.js
+++ src/context/MenuContext.js
@@ -1,14 +1,16 @@
-import React , {createContext, useContext, useState} from 'react'
+import React, { createContext, useContext, useState } from "react";
 
 const MenuContext = createContext();
 
-export default MenuContextProvider = ({children}) => {
-   const [openMenu, setOpenMenu] = useState(false);
-   return (
-     <MenuContext.Provider values={[openMenu, setOpenMenu]}>
+const MenuContextProvider = ({ children }) => {
+  const [openMenu, setOpenMenu] = useState(false);
+  return (
+    <MenuContext.Provider value={[openMenu, setOpenMenu]}>
       {children}
-     </MenuContext.Provider>
-    );
-}
+    </MenuContext.Provider>
+  );
+};
 
 export const useMenu = () => useContext(MenuContext);
+
+export default MenuContextProvider;
