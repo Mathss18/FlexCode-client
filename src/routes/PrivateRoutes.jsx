71c5768e8ea378bd1a3d6bad73472002a0fc9edb
unknown <theus.7@hotmail.com>
2021-10-16 20:22:17 -0300

    Resvendo problema da 1a request adicionando Interceptor

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -2,16 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 function PrivateRoutes ({ Component, ...rest }) {
-    console.log(Component);
-    console.log(rest)
     const [token, setToken] = useState(localStorage.getItem('token'));
 
     useEffect(() => {
        setToken(localStorage.getItem('token'));
 
-    }, [localStorage.getItem('token')]);
-    
-    console.log('Token:' + token);
+    }, []);
 
     if (token == null) {
         return <Redirect to="/login" />
