33710a51d3a7719a5b3bf635a5a93f300352c8fb
Matheus Valle <matheusvalle_vni@outlook.com>
2021-10-27 23:51:18 -0300

    Criando a tela de variações e configurando formik

--- src/services/validationSchema.js
+++ src/services/validationSchema.js
@@ -2,13 +2,22 @@ import * as yup from 'yup';
 
 const validationSchema = yup.object({
   email: yup
-    .string('Enter your email')
-    .email('Enter a valid email')
-    .required('Email is required'),
+    .string('Insira seu email')
+    .email('Insira um email válido')
+    .required('Este campo é obrigatório'),
   password: yup
-    .string('Enter your password')
-    .min(8, 'Password should be of minimum 8 characters length')
-    .required('Password is required'),
+    .string('Insira sua senha')
+    .min(8, 'São necessários no minímo 8 caracteres para a senha')
+    .required('Este campo é obrigatório'),
+  cpf: yup
+    .string('Insira seu CPF')
+    .min(11, 'São necessários no minímo 11 caracteres para o CPF')
+    .required('Este campo é obrigatório'),
+  phone: yup
+    .string('Insira seu telefone')
+    .min(10, 'São necessários no minímo 10 caracteres para o telefone')
+    .required('Este campo é obrigatório'),
+
 });
 
 export default validationSchema;
