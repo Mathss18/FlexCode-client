502bd2fd2d017fa46ab53748ebb4229c18b70c97
Allanda Soares da Mata Araujo <20298@mtcorp.com.br>
2021-12-29 10:45:02 -0300

    Primeiro commit

--- src/pages/Produtos/gerenciarProdutos/Tabs/Valores.jsx
+++ src/pages/Produtos/gerenciarProdutos/Tabs/Valores.jsx
@@ -138,7 +138,7 @@ export function Valores() {
     <Grid container spacing={3}>
       <Grid item xs={6}>
         <TextField variant="outlined" style={{ marginBottom: 24 }} label="Valor de Custo" required fullWidth value={values.valor_custo} name="valor_custo" onChange={handleOnChange} />
-        <TextField variant="outlined" style={{ marginBottom: 24 }} label="Despesas acessórias" fullWidth value={values.despesa_acessoria} name="despesa_acessoria" onChange={handleOnChange} />
+        <TextField variant="outlined" style={{ marginBottom: 24 }} label="Despesas adicionais" fullWidth value={values.despesa_acessoria} name="despesa_acessoria" onChange={handleOnChange} />
         <TextField variant="outlined" style={{ marginBottom: 24 }} label="Outras despesas" fullWidth value={values.outras_despesas} name="outras_despesas" onChange={handleOnChange} />
         <TextField variant="outlined" label="Custo final" fullWidth value={values.custo_final} name="custo_final" onChange={handleOnChange} />
       </Grid>
