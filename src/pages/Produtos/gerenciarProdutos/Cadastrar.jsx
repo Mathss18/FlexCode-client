9411d2e83c8d46d29bbd55308992710b40098b2c
unknown <theus.7@hotmail.com>
2022-01-18 20:01:14 -0300

    refazendo tela de produtos

--- src/pages/Produtos/gerenciarProdutos/Cadastrar.jsx
+++ src/pages/Produtos/gerenciarProdutos/Cadastrar.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext } from "react";
-import { Grid, Divider, Button, Box, Tabs, Tab } from "@material-ui/core";
+import { Grid, Divider, Button, Box, Tabs, Tab, AppBar } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
-import { GerenciarProdutosContext } from "../../../context/GerenciarProdutosContext";
+import { useProdutoContext } from "../../../context/GerenciarProdutosContext";
 import api from "../../../services/api";
 import CheckIcon from "@material-ui/icons/Check";
 import CloseIcon from "@material-ui/icons/Close";
@@ -10,144 +10,49 @@ import { Dados, Detalhes, Valores, Estoque, Fotos, Fiscal, Fornecedores, Composi
 
 function CadastrarProduto() {
   const history = useHistory();
-  const [value, setValue] = useState(0);
-  const { values } = useContext(GerenciarProdutosContext);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  // const handleOnChange = (e) => {
-  //   let { name, value, id } = e.target;
-
-  //   console.log(e);
-
-  //   if (name === "variacoes") {
-  //     const index = id.split("_")[1];
-  //     const newVariacoes = [...values.variacoes];
-  //     newVariacoes[index] = value;
-  //     console.log(newVariacoes);
-  //     setValues({ ...values, variacoes: newVariacoes });
-  //   } else {
-  //     setValues({ ...values, [name]: value });
-  //   }
-  // };
+  const [currentTab, setCurrentTab] = useState(0);
 
   function TabPanel(props) {
     const { children, value, index, ...other } = props;
 
     return (
-      <Grid role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
-        {value === index && <Box p={3}>{children}</Box>}
-      </Grid>
+      <div>
+        {value === index && children}
+      </div>
     );
   }
 
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      "aria-controls": `simple-tabpanel-${index}`,
-    };
+  function handleTabChange(event, val) {
+    setCurrentTab(val);
   }
 
-  // function handleOnSubmit(e) {
-  //   e.preventDefault();
-
-  //   api
-  //     .post("/tipo-variacao-produto", { nome: values.nome })
-  //     .then((res) => {
-  //       console.log("@VARIACAO-PRODUTO", res.data);
-  //       values.variacoes.forEach((variacao) => {
-  //         api
-  //           .post("/nome-variacao-produto", {
-  //             nome: variacao,
-  //             tipo_variacao_produto_id: res.data.data.id,
-  //             tipo_variacao_produto: res.data.data,
-  //           })
-  //           .catch((err) => {
-  //             console.log(err);
-  //           });
-  //       });
-  //     })
-  //     .catch((err) => {
-  //       console.log("@ERROR", err);
-  //     });
-  // }
-
   return (
+
     <>
-        <div>
-          <Divider />
-          <div style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}>
-            <AssignmentIcon />
-            <h3>Dados da Unidade de Produtos</h3>
-          </div>
-          <form>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                  <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                    <Tab label="Dados" {...a11yProps(0)} />
-                    <Tab label="Detalhes" {...a11yProps(1)} />
-                    <Tab label="Valores" {...a11yProps(2)} />
-                    <Tab disabled={values.movimenta_estoque === 0} label="Estoque" {...a11yProps(3)} />
-                    <Tab label="Fotos" {...a11yProps(4)} />
-                    <Tab disabled={values.habilitar_nota_fiscal === 0} label="Fiscal" {...a11yProps(5)} />
-                    <Tab label="Fornecedores" {...a11yProps(6)} />
-                  </Tabs>
-                </Box>
-              </Grid>
-            </Grid>
-
-            <form>
-              <TabPanel value={value} index={0}>
-                <Dados />
-              </TabPanel>
-              <TabPanel value={value} index={1}>
-                <Detalhes />
-              </TabPanel>
-              <TabPanel value={value} index={2}>
-                <Valores />
-              </TabPanel>
-              <TabPanel value={value} index={3}>
-                <Estoque />
-              </TabPanel>
-              <TabPanel value={value} index={4}>
-                <Fotos />
-              </TabPanel>
-              <TabPanel value={value} index={5}>
-                <Fiscal />
-              </TabPanel>
-              <TabPanel value={value} index={6}>
-                <Fornecedores />
-              </TabPanel>
-            </form>
-
-            <Box sx={{ textAlign: "center" }} spacing={2}>
-              <Button disabled={value <= 0} className={"btn btn-primary btn-spacing"} variant="outlined" onClick={() => setValue(value - 1)}>
-                Voltar
-              </Button>
-              <Button className={"btn btn-primary btn-spacing"} variant="outlined" onClick={() => setValue(value + 1)}>
-                Continuar
-              </Button>
-            </Box>
-            <br />
-            <Divider />
-
-            <Grid container spacing={0}>
-              <Grid item>
-                <Button type="submit" variant="outlined" startIcon={<CheckIcon />} className={"btn btn-primary btn-spacing"}>
-                  Salvar
-                </Button>
-              </Grid>
-              <Grid item>
-                <Button onClick={() => history.push("/produtos")} variant="outlined" startIcon={<CloseIcon />} className={"btn btn-error btn-spacing"}>
-                  Cancelar
-                </Button>
-              </Grid>
-            </Grid>
-          </form>
+      <div>
+        <Divider />
+        <div style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}>
+          <AssignmentIcon />
+          <h3>Dados da Unidade de Produtos</h3>
         </div>
+        <Tabs style={{marginBottom: '24px'}} value={currentTab} onChange={handleTabChange}>
+          <Tab label="Dados" icon={<AssignmentIcon />} />
+          <Tab label="Detalhes" icon={<AssignmentIcon />} />
+          <Tab label="Valores" icon={<AssignmentIcon />} />
+          <Tab label="Estoque" icon={<AssignmentIcon />} />
+          <Tab label="Fotos" icon={<AssignmentIcon />} />
+          <Tab label="Fiscal" icon={<AssignmentIcon />} />
+          <Tab label="Fornecedores" icon={<AssignmentIcon />} />
+        </Tabs>
+
+        <TabPanel value={currentTab} index={0}><Dados /></TabPanel>
+        <TabPanel value={currentTab} index={1}><Detalhes /></TabPanel>
+        <TabPanel value={currentTab} index={2}><Valores /></TabPanel>
+        <TabPanel value={currentTab} index={3}><Estoque /></TabPanel>
+        <TabPanel value={currentTab} index={4}><Fotos /></TabPanel>
+        <TabPanel value={currentTab} index={5}><Fiscal /></TabPanel>
+        <TabPanel value={currentTab} index={6}><Fornecedores /></TabPanel>
+      </div>
     </>
   );
 }
--- src/pages/Produtos/gerenciarProdutos/Tabs/Fiscal.jsx
+++ src/pages/Produtos/gerenciarProdutos/Tabs/Fiscal.jsx
@@ -1,39 +1,115 @@
-import { useContext } from "react";
-import { GerenciarProdutosContext } from "../../../../context/GerenciarProdutosContext";
-import { Grid, TextField, Typography, Divider } from "@material-ui/core";
+import { useProdutoContext } from "../../../../context/GerenciarProdutosContext";
+import { Grid, TextField, Divider } from "@material-ui/core";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import OpenWithIcon from "@mui/icons-material/OpenWith";
 
 export function Fiscal() {
-  const { values, setValues } = useContext(GerenciarProdutosContext);
+  const produtoContext = useProdutoContext();
 
   function handleOnChange(event) {
     const { name, value } = event.target;
-    setValues({ ...values, [name]: value });
+    produtoContext.useValues.setValues({ ...produtoContext.useValues.values, [name]: value });
+    produtoContext.formik.setFieldValue(name, value);
+    console.log(produtoContext.formik.values);
   }
 
   return (
     <>
+      <div style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}>
+        <OpenWithIcon />
+        <h3>Detalhes</h3>
+      </div>
       <Grid container spacing={3}>
         <Grid item xs={4}>
-          <TextField variant="outlined" label="NCM" fullWidth value={values.ncm} name="ncm" onChange={handleOnChange} />
+          <TextField
+            variant="outlined"
+            label="NCM"
+            fullWidth
+            value={produtoContext.useValues.values.ncm}
+            name="ncm"
+            onChange={handleOnChange}
+            onBlur={produtoContext.formik.handleBlur}
+            error={produtoContext.formik.touched.ncm && Boolean(produtoContext.formik.errors.ncm)}
+            helperText={produtoContext.formik.touched.ncm && produtoContext.formik.errors.ncm}
+          />
         </Grid>
         <Grid item xs={4}>
-          <TextField variant="outlined" label="CEST" fullWidth value={values.cest} name="cest" onChange={handleOnChange} />
+          <TextField
+            variant="outlined"
+            label="CEST"
+            fullWidth
+            value={produtoContext.useValues.values.cest}
+            name="cest"
+            onChange={handleOnChange}
+            onBlur={produtoContext.formik.handleBlur}
+            error={produtoContext.formik.touched.cest && Boolean(produtoContext.formik.errors.cest)}
+            helperText={produtoContext.formik.touched.cest && produtoContext.formik.errors.cest}
+          />
         </Grid>
         <Grid item xs={4}>
-          <TextField variant="outlined" label="Origem" fullWidth value={values.origem} name="origem" onChange={handleOnChange} />
+          <TextField
+            variant="outlined"
+            label="Origem"
+            fullWidth
+            value={produtoContext.useValues.values.origem}
+            name="origem"
+            onChange={handleOnChange}
+            onBlur={produtoContext.formik.handleBlur}
+            error={produtoContext.formik.touched.origem && Boolean(produtoContext.formik.errors.origem)}
+            helperText={produtoContext.formik.touched.origem && produtoContext.formik.errors.origem}
+          />
         </Grid>
         <Grid item xs={3}>
-          <TextField variant="outlined" label="Peso líquido" fullWidth value={values.pesoLiquido} name="pesoLiquido" onChange={handleOnChange} />
+          <TextField
+            variant="outlined"
+            label="Peso líquido"
+            fullWidth
+            value={produtoContext.useValues.values.pesoLiquido}
+            name="pesoLiquido"
+            onChange={handleOnChange}
+            onBlur={produtoContext.formik.handleBlur}
+            error={produtoContext.formik.touched.pesoLiquido && Boolean(produtoContext.formik.errors.pesoLiquido)}
+            helperText={produtoContext.formik.touched.pesoLiquido && produtoContext.formik.errors.pesoLiquido}
+          />
         </Grid>
         <Grid item xs={3}>
-          <TextField variant="outlined" label="Peso bruto" fullWidth value={values.pesoBruto} name="pesoBruto" onChange={handleOnChange} />
+          <TextField
+            variant="outlined"
+            label="Peso bruto"
+            fullWidth
+            value={produtoContext.useValues.values.pesoBruto}
+            name="pesoBruto"
+            onChange={handleOnChange}
+            onBlur={produtoContext.formik.handleBlur}
+            error={produtoContext.formik.touched.pesoBruto && Boolean(produtoContext.formik.errors.pesoBruto)}
+            helperText={produtoContext.formik.touched.pesoBruto && produtoContext.formik.errors.pesoBruto}
+          />
         </Grid>
         <Grid item xs={3}>
-          <TextField variant="outlined" label="Número FCI" fullWidth value={values.numeroFci} name="numeroFci" onChange={handleOnChange} />
+          <TextField
+            variant="outlined"
+            label="Número FCI"
+            fullWidth
+            value={produtoContext.useValues.values.numeroFci}
+            name="numeroFci"
+            onChange={handleOnChange}
+            onBlur={produtoContext.formik.handleBlur}
+            error={produtoContext.formik.touched.numeroFci && Boolean(produtoContext.formik.errors.numeroFci)}
+            helperText={produtoContext.formik.touched.numeroFci && produtoContext.formik.errors.numeroFci}
+          />
         </Grid>
         <Grid item xs={3}>
-          <TextField variant="outlined" label="% Vr. aprox. tribut." fullWidth value={values.valorAproxTribut} name="valorAproxTribut" onChange={handleOnChange} />
+          <TextField
+            variant="outlined"
+            label="% Vr. aprox. tribut."
+            fullWidth
+            value={produtoContext.useValues.values.valorAproxTribut}
+            name="valorAproxTribut"
+            onChange={handleOnChange}
+            onBlur={produtoContext.formik.handleBlur}
+            error={produtoContext.formik.touched.valorAproxTribut && Boolean(produtoContext.formik.errors.valorAproxTribut)}
+            helperText={produtoContext.formik.touched.valorAproxTribut && produtoContext.formik.errors.valorAproxTribut}
+          />
         </Grid>
       </Grid>
       <br />
@@ -44,16 +120,56 @@ export function Fiscal() {
       </div>
       <Grid container spacing={3}>
         <Grid item xs={3}>
-          <TextField variant="outlined" label="Valor fixo PIS" fullWidth value={values.valorPixoPis} name="valorPixoPis" onChange={handleOnChange} />
+          <TextField
+            variant="outlined"
+            label="Valor fixo PIS"
+            fullWidth
+            value={produtoContext.useValues.values.valorPixoPis}
+            name="valorPixoPis"
+            onChange={handleOnChange}
+            onBlur={produtoContext.formik.handleBlur}
+            error={produtoContext.formik.touched.valorPixoPis && Boolean(produtoContext.formik.errors.valorPixoPis)}
+            helperText={produtoContext.formik.touched.valorPixoPis && produtoContext.formik.errors.valorPixoPis}
+          />
         </Grid>
         <Grid item xs={3}>
-          <TextField variant="outlined" label="Valor fixo PIS ST" fullWidth value={values.valorFixoPisSt} name="valorFixoPisSt" onChange={handleOnChange} />
+          <TextField
+            variant="outlined"
+            label="Valor fixo PIS ST"
+            fullWidth
+            value={produtoContext.useValues.values.valorFixoPisSt}
+            name="valorFixoPisSt"
+            onChange={handleOnChange}
+            onBlur={produtoContext.formik.handleBlur}
+            error={produtoContext.formik.touched.valorFixoPisSt && Boolean(produtoContext.formik.errors.valorFixoPisSt)}
+            helperText={produtoContext.formik.touched.valorFixoPisSt && produtoContext.formik.errors.valorFixoPisSt}
+          />
         </Grid>
         <Grid item xs={3}>
-          <TextField variant="outlined" label="Valor fixo COFINS" fullWidth value={values.valorFixoCofins} name="valorFixoCofins" onChange={handleOnChange} />
+          <TextField
+            variant="outlined"
+            label="Valor fixo COFINS"
+            fullWidth
+            value={produtoContext.useValues.values.valorFixoCofins}
+            name="valorFixoCofins"
+            onChange={handleOnChange}
+            onBlur={produtoContext.formik.handleBlur}
+            error={produtoContext.formik.touched.valorFixoCofins && Boolean(produtoContext.formik.errors.valorFixoCofins)}
+            helperText={produtoContext.formik.touched.valorFixoCofins && produtoContext.formik.errors.valorFixoCofins}
+          />
         </Grid>
         <Grid item xs={3}>
-          <TextField variant="outlined" label="Valor fixo COFINS ST" fullWidth value={values.valorFixoCofinsSt} name="valorFixoCofinsSt" onChange={handleOnChange} />
+          <TextField
+            variant="outlined"
+            label="Valor fixo COFINS ST"
+            fullWidth
+            value={produtoContext.useValues.values.valorFixoCofinsSt}
+            name="valorFixoCofinsSt"
+            onChange={handleOnChange}
+            onBlur={produtoContext.formik.handleBlur}
+            error={produtoContext.formik.touched.valorFixoCofinsSt && Boolean(produtoContext.formik.errors.valorFixoCofinsSt)}
+            helperText={produtoContext.formik.touched.valorFixoCofinsSt && produtoContext.formik.errors.valorFixoCofinsSt}
+          />
         </Grid>
       </Grid>
     </>
