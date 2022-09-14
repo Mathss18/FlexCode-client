669779ee1ca0e964ef52b1101a80dc90f39352c6
Allanda <allandasoares7@gmail.com>
2022-09-14 13:36:47 -0300

    Altera o chip de fornecedores na listagem de produtos

--- src/pages/Produtos/gerenciar-produtos/Listar.jsx
+++ src/pages/Produtos/gerenciar-produtos/Listar.jsx
@@ -9,6 +9,7 @@ import EditIcon from "@material-ui/icons/Edit";
 import SearchIcon from "@material-ui/icons/Search";
 import { useFullScreenLoader } from "../../../context/FullScreenLoaderContext";
 import { useProdutoContext } from "../../../context/GerenciarProdutosContext";
+import { Chip } from "@mui/material";
 
 function ListarProdutos() {
   const history = useHistory();
@@ -67,45 +68,74 @@ function ListarProdutos() {
 
   useEffect(() => {
     fullScreenLoader.setLoading(true);
-    api.get("/produtos-mini")
+    api
+      .get("/produtos-mini")
       .then((response) => {
-          response.data["data"].forEach((element) => {
-            var array = [
-              element["nome"],
-              element["codigoInterno"],
-              element["grupo_produto"]["nome"],
-              `R$: ${element['custoFinal'].toFixed(empresaConfig.quantidadeCasasDecimaisValor)}`,
-              `${element["quantidadeAtual"]} ${element["unidade_produto"]?.sigla ?? ''}`,
-              element?.cliente?.nome,
-              element["fornecedores"].map((item,index)=>{
-                if(element["fornecedores"].length > 3){
-                  let string = '';
-                  if(index < 3){
-                    string += item["nome"] + ', ';
-                    if(index === 2){
-                      string += '... + ' + (element["fornecedores"].length - 3) + ' fornecedores';
-                    }
-                    return string;
-                  }
-                }
-                else{
-                  return item["nome"] + ', ';
-                }
-              }),
-              new Date(element["created_at"]).toLocaleString(),
-              <>
-                {/* <SearchIcon className={"btn-lista"} onClick={(event) => handleOnClickShowButton(event, element["id"])} /> */}
-                <EditIcon className={"btn-lista"} onClick={(event) => handleOnClickEditButton(event, element["id"])} />
-              </>,
-            ];
-            data.push(array);
-          });
+        response.data["data"].forEach((element) => {
+          var qtdFornecedores = element?.fornecedores?.length - 1;
+          // console.log("Element", element.fornecedores.length)
+          var array = [
+            element["nome"],
+            element["codigoInterno"],
+            element["grupo_produto"]["nome"],
+            `R$: ${element["custoFinal"].toFixed(
+              empresaConfig.quantidadeCasasDecimaisValor
+            )}`,
+            `${element["quantidadeAtual"]} ${
+              element["unidade_produto"]?.sigla ?? ""
+            }`,
+            element?.cliente?.nome,
 
-          setGrupos(data);
+            // element["fornecedores"].map((item,index)=>{
+            //   if(element["fornecedores"].length > 3){
+            //     let string = '';
+            //     if(index < 3){
+            //       string += item["nome"] + ', ';
+            //       if(index === 2){
+            //         string += '... + ' + (element["fornecedores"].length - 3) + ' fornecedores';
+            //       }
+            //       return string;
+            //     }
+            //   }
+            //   else{
+            //     return item["nome"] + ', ';
+            //   }
+            // }),
+
+            element?.fornecedores?.length === 0 ? null : (
+              <>
+                <Chip
+                  label={element?.fornecedores[0]?.nome}
+                  style={{ background: "#585858" }}
+                />
+                {qtdFornecedores === 0 ? null : (
+                  <Chip
+                    label={"+" + qtdFornecedores}
+                    style={{ background: "#585858", marginLeft: 2 }}
+                  />
+                )}
+              </>
+            ),
+
+            new Date(element["created_at"]).toLocaleString(),
+            <>
+              {/* <SearchIcon className={"btn-lista"} onClick={(event) => handleOnClickShowButton(event, element["id"])} /> */}
+              <EditIcon
+                className={"btn-lista"}
+                onClick={(event) =>
+                  handleOnClickEditButton(event, element["id"])
+                }
+              />
+            </>,
+          ];
+          data.push(array);
+        });
+
+        setGrupos(data);
       })
       .finally(() => {
         fullScreenLoader.setLoading(false);
-      })
+      });
   }, []);
 
   useEffect(() => {
@@ -114,10 +144,21 @@ function ListarProdutos() {
 
   return (
     <>
-      <Button onClick={() => history.push("/produtos/novo")} variant="outlined" startIcon={<AddIcon />} className={"btn btn-primary btn-spacing"}>
+      <Button
+        onClick={() => history.push("/produtos/novo")}
+        variant="outlined"
+        startIcon={<AddIcon />}
+        className={"btn btn-primary btn-spacing"}
+      >
         Adicionar
       </Button>
-      <MUIDataTable title={"Lista de Produtos"} data={grupos} columns={columns} options={config} className={"table-background"} />
+      <MUIDataTable
+        title={"Lista de Produtos"}
+        data={grupos}
+        columns={columns}
+        options={config}
+        className={"table-background"}
+      />
     </>
   );
 }
