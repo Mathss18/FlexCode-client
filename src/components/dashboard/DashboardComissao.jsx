52826b47f1f211b9e41c598bde22ece902054c79
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2024-04-10 20:30:01 -0300

    push changes

--- src/components/dashboard/DashboardComissao.jsx
+++ src/components/dashboard/DashboardComissao.jsx
@@ -4,13 +4,13 @@ function DashboardComissao({ dados }) {
             return `Este mês estamos operando ${dados?.diferencaPercentual?.toFixed(2)?.replace('.', ',')}% acima da média.`
         }
         else if (dados?.diferencaPercentual < 0) {
-            return `Este mês estamos operando -${dados?.diferencaPercentual?.toFixed(2)?.replace('.', ',')}% abaixo da média.`
+            return `Este mês estamos operando ${dados?.diferencaPercentual?.toFixed(2)?.replace('.', ',')}% abaixo da média.`
         }
         else {
             return `Este mês estamos operando no zero a zero.`
         }
     }
-    return <div>{getTexto()}</div>
+    return <div style={{ fontSize: 14 }}>{getTexto()}</div>
 }
 
 export default DashboardComissao;
